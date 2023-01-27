d9f54771523fe013791f8d8e7b31d3eb2517acc6
Meruem117 <1572677319@qq.com>
2023-01-27 14:58:13 +0800

    read

--- examples/main/multiple.js
+++ examples/main/multiple.js
@@ -1,19 +1,30 @@
-import { loadMicroApp } from '../../es';
+import {
+    loadMicroApp
+} from '../../es'
 
-let app;
+let app
 
 function mount() {
-  app = loadMicroApp(
-    { name: 'react15', entry: '//localhost:7102', container: '#react15' },
-    { sandbox: { experimentalStyleIsolation: true } },
-  );
+    app = loadMicroApp({
+        name: 'react15',
+        entry: '//localhost:7102',
+        container: '#react15'
+    }, {
+        sandbox: {
+            experimentalStyleIsolation: true
+        }
+    })
 }
 
 function unmount() {
-  app.unmount();
+    app.unmount()
 }
 
-document.querySelector('#mount').addEventListener('click', mount);
-document.querySelector('#unmount').addEventListener('click', unmount);
+document.querySelector('#mount').addEventListener('click', mount)
+document.querySelector('#unmount').addEventListener('click', unmount)
 
-loadMicroApp({ name: 'vue', entry: '//localhost:7101', container: '#vue' });
+loadMicroApp({
+    name: 'vue',
+    entry: '//localhost:7101',
+    container: '#vue'
+})
--- examples/main/render/VueRender.js
+++ examples/main/render/VueRender.js
@@ -1,28 +1,34 @@
-import Vue from 'vue/dist/vue.esm';
+import Vue from 'vue/dist/vue.esm'
 
-function vueRender({ loading }) {
-  return new Vue({
-    template: `
-      <div id="subapp-container">
-        <h4 v-if="loading" class="subapp-loading">Loading...</h4>
-        <div id="subapp-viewport"></div>
-      </div>
-    `,
-    el: '#subapp-container',
-    data() {
-      return {
-        loading,
-      };
-    },
-  });
+function vueRender({
+    loading
+}) {
+    return new Vue({
+        template: `
+          <div id="subapp-container">
+            <h4 v-if="loading" class="subapp-loading">Loading...</h4>
+            <div id="subapp-viewport"></div>
+          </div>
+        `,
+        el: '#subapp-container',
+        data() {
+            return {
+                loading
+            }
+        },
+    })
 }
 
-let app = null;
+let app = null
 
-export default function render({ loading }) {
-  if (!app) {
-    app = vueRender({ loading });
-  } else {
-    app.loading = loading;
-  }
+export default function render({
+    loading
+}) {
+    if (!app) {
+        app = vueRender({
+            loading
+        })
+    } else {
+        app.loading = loading
+    }
 }
