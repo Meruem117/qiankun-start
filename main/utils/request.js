312bbe2ef50d9e8b187184b34c81feb2a517c234
Meruem117 <1572677319@qq.com>
2023-02-01 21:38:03 +0800

    update request

--- main/utils/request.js
+++ main/utils/request.js
@@ -3,7 +3,52 @@ import axios from 'axios'
 const baseUrl = 'http://localhost:8080'
 
 export default {
-    get: function () {
-        console.log(1)
+    get: function (url, params) {
+        return new Promise((resolve, reject) => {
+            axios.get(baseUrl + url, {
+                    params: params,
+                })
+                .then(res => {
+                    resolve(res.data)
+                })
+                .catch(err => {
+                    reject(err.data)
+                })
+        })
+    },
+    post: function (url, params) {
+        return new Promise((resolve, reject) => {
+            axios.post(baseUrl + url, params)
+                .then(res => {
+                    resolve(res.data)
+                })
+                .catch(err => {
+                    reject(err.data)
+                })
+        })
+    },
+    put: function (url, params) {
+        return new Promise((resolve, reject) => {
+            axios.put(baseUrl + url, params)
+                .then(res => {
+                    resolve(res.data)
+                })
+                .catch(err => {
+                    reject(err.data)
+                })
+        })
+    },
+    delete: function (url, params) {
+        return new Promise((resolve, reject) => {
+            axios.delete(baseUrl + url, {
+                    params: params
+                })
+                .then(res => {
+                    resolve(res.data)
+                })
+                .catch(err => {
+                    reject(err.data)
+                })
+        })
     }
 }
