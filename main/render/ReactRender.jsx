59d52aea40af1338a9ae09ae1bca5c6b08879d6e
Meruem117 <1572677319@qq.com>
2023-02-08 21:58:50 +0800

    reformat

--- main/render/ReactRender.jsx
+++ main/render/ReactRender.jsx
@@ -5,17 +5,17 @@ import ReactDOM from 'react-dom'
  * 渲染子应用
  */
 function Render(props) {
-  const { loading } = props
+    const { loading } = props
 
-  return (
-    <>
-      {loading && <h4 className="subapp-loading">Loading...</h4>}
-      <div id="subapp-viewport" />
-    </>
-  )
+    return (
+        <>
+            {loading && <h4 className="subapp-loading">Loading...</h4>}
+            <div id="subapp-viewport" />
+        </>
+    )
 }
 
 export default function render({ loading }) {
-  const container = document.getElementById('subapp-container')
-  ReactDOM.render(<Render loading={loading} />, container)
+    const container = document.getElementById('subapp-container')
+    ReactDOM.render(<Render loading={loading} />, container)
 }
