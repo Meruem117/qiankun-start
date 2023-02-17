0e59badeb8a907aa95e0c5a9f414f17c4d4519a0
Meruem117 <1572677319@qq.com>
2023-02-17 22:50:02 +0800

    update

--- main-mobile/index.js
+++ main-mobile/index.js
@@ -1,23 +1,13 @@
 import {
-    initGlobalState,
     registerMicroApps,
-    runAfterFirstMounted,
     setDefaultMountApp,
     start
 } from 'qiankun'
 import request from './utils/request'
 import './index.less'
 
-/**
- * 主应用 **可以使用任意技术栈**
- * 以下分别是 React 和 Vue 的示例，可切换尝试
- */
-// import render from './render/ReactRender'
 import render from './render/VueRender'
 
-/**
- * Step1 初始化应用（可选）
- */
 render({
     loading: true
 })
@@ -26,9 +16,6 @@ const loader = (loading) => render({
     loading
 })
 
-/**
- * Step2 注册子应用
- */
 registerMicroApps(
     [{
         name: 'vue',
@@ -55,34 +42,9 @@ registerMicroApps(
                 console.log('[LifeCycle] after unmount %c%s', 'color: green;', app.name)
             },
         ],
-    })
+    }
+)
 
-const {
-    onGlobalStateChange,
-    setGlobalState
-} = initGlobalState({
-    user: 'qiankun',
-})
-
-onGlobalStateChange((value, prev) => console.log('[onGlobalStateChange - master]:', value, prev))
-
-setGlobalState({
-    ignore: 'master',
-    user: {
-        name: 'master',
-    },
-})
-
-/**
- * Step3 设置默认进入的子应用
- */
 setDefaultMountApp('/vue')
 
-/**
- * Step4 启动应用
- */
 start()
-
-runAfterFirstMounted(() => {
-    console.log('[MainApp] first app mounted')
-})
